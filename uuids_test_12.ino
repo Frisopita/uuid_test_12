b7271c55559b9a0ec19ed6e0125249b83444228e
Frida Andrade <f.andrade@s-k.dev>
2024-05-26 19:29:23 -0600

    se realizan ultimas modificaciones al código

--- uuids_test_12.ino
+++ uuids_test_12.ino
@@ -7,12 +7,14 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
-#include <Thread.h>
-#include <ThreadController.h>
 
-ThreadController controll = ThreadController();
+#define DEVICE_NAME "SMART_SOCKS"
+//#include <Thread.h>
+//#include <ThreadController.h>
 
-Thread* btThread = new Thread();
+//ThreadController controll = ThreadController();
+
+//Thread* btThread = new Thread();
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_S1_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
@@ -27,7 +29,6 @@ Thread* btThread = new Thread();
 #define CHARACTERISTIC_S10_UUID "a8f2dbc3-c562-42d9-a094-33e4cca73118"
 #define CHARACTERISTIC_S11_UUID "3c21b038-85a3-4c47-aa78-446f301dd61c"
 #define CHARACTERISTIC_S12_UUID "1b0724f2-156b-41a6-8bb6-22be491731fc"
-#define CHARACTERISTIC_S13_UUID "6743b155-5778-4756-b31f-34e9e8e53a9d"
 
 BLEServer* pServer = NULL;
 BLECharacteristic* pCharacteristicS1 = NULL;
@@ -42,25 +43,25 @@ BLECharacteristic* pCharacteristicS9 = NULL;
 BLECharacteristic* pCharacteristicS10 = NULL;
 BLECharacteristic* pCharacteristicS11 = NULL;
 BLECharacteristic* pCharacteristicS12 = NULL;
-BLECharacteristic* pCharacteristicS13 = NULL;
 
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 uint32_t value = 0;
 
-int S1 = 120;
-int S2 = 100;
-int S3 = 40;
+//TEMPERATIRAS
+
+int S1 = 38;
+int S2 = 23;
+int S3 = 25;
 int S4 = 25;
-int S5 = 35;
-int S6 = 10;
-int S7 = 7;
-int S8 = 15;
-int S9 = 50;
-int S10 = 65;
-int S11 = 75;
-int S12 = 85;
-int S13 = 0;
+int S5 = 24;
+int S6 = 25;
+int S7 = 23;
+int S8 = 24;
+int S9 = 25;
+int S10 = 25;
+int S11 = 26;
+int S12 = 35;
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -83,7 +84,9 @@ class MyCallbacks: public BLECharacteristicCallbacks{
       Serial.print("Datos:");
 
       for (int i = 0; i < rxValue.length(); i++){
-        Serial.print(rxValue[i]);
+        int value = rxValue[i];
+        Serial.print(',');
+        Serial.print(value);
       }
 
       Serial.println();
@@ -96,19 +99,19 @@ void setup() {
   Serial.begin(115200);
   initBT();
   
-  btThread->onRun(btCallback);
-  btThread->setInterval(100);
+  //btThread->onRun(btCallback);
+  //btThread->setInterval(100);
 
-  controll.add(btThread);
+ // controll.add(btThread);
 }
 
 void loop() {
 // put your main code here, to run repeatedly: 
-  controll.run();
-
+  //controll.run();
+  btCallback();
      //Aumentar los valores de los sensores
-     S1 = S1+1;
-     S2 = S2+1;
+     //S1 = S1+1;
+     //S2 = S2+1;
       
       delay(5000);
 
@@ -135,11 +138,7 @@ void loop() {
       Serial.print(" S11:");
       Serial.print(S11);
       Serial.print(" S12:");
-      Serial.print(S12);
-      Serial.print(" S13:");
-      Serial.print(S13);
-      Serial.print('\n');
-  
+      Serial.print(S12);  
 }
 
 void btCallback(){
@@ -170,8 +169,6 @@ void btCallback(){
       strS11 += S11;
       String strS12 = "";
       strS12 += S12;
-      String strS13 = "";
-      strS12 += S13;
       
       Serial.print(strS1);
       Serial.print(strS2);
@@ -185,7 +182,6 @@ void btCallback(){
       Serial.print(strS10);
       Serial.print(strS11);
       Serial.print(strS12);
-      Serial.print(strS13);
       
       pCharacteristicS1->setValue((char*)strS1.c_str());
       pCharacteristicS1->notify();
@@ -222,10 +218,6 @@ void btCallback(){
 
       pCharacteristicS12->setValue((char*)strS12.c_str());
       pCharacteristicS12->notify();
-    
-      pCharacteristicS13->setValue((char*)strS13.c_str());
-      pCharacteristicS13->notify();
-
     }
     // disconnecting
     if (!deviceConnected && oldDeviceConnected) {
@@ -244,7 +236,7 @@ void btCallback(){
 
 void initBT(){
   // Create the BLE Device
-  BLEDevice::init("ESP32 Sopita");
+  BLEDevice::init(DEVICE_NAME);
 
   // Create the BLE Server
   pServer = BLEDevice::createServer();
@@ -349,14 +341,7 @@ void initBT(){
                       BLECharacteristic::PROPERTY_NOTIFY |
                       BLECharacteristic::PROPERTY_INDICATE
                     );
-  
-  pCharacteristicS13 = pService->createCharacteristic(
-                      CHARACTERISTIC_S13_UUID,
-                      BLECharacteristic::PROPERTY_READ   |
-                      BLECharacteristic::PROPERTY_WRITE  |
-                      BLECharacteristic::PROPERTY_NOTIFY |
-                      BLECharacteristic::PROPERTY_INDICATE
-                    );
+
   
   pCharacteristicS1->setCallbacks(new MyCallbacks());
   pCharacteristicS2->setCallbacks(new MyCallbacks());
@@ -370,7 +355,6 @@ void initBT(){
   pCharacteristicS10->setCallbacks(new MyCallbacks());
   pCharacteristicS11->setCallbacks(new MyCallbacks());
   pCharacteristicS12->setCallbacks(new MyCallbacks());
-  pCharacteristicS13->setCallbacks(new MyCallbacks());
   // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
   // Create a BLE Descriptorn
   pCharacteristicS1->addDescriptor(new BLE2902());
@@ -385,7 +369,6 @@ void initBT(){
   pCharacteristicS10->addDescriptor(new BLE2902());
   pCharacteristicS11->addDescriptor(new BLE2902());
   pCharacteristicS12->addDescriptor(new BLE2902());
-  pCharacteristicS13->addDescriptor(new BLE2902());
   // Start the service
   pService->start();
 
